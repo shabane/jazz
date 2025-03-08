792e3471b5d3d174c0a4ad611af6ce7b036a115d
shabane <m.mohamadshabane@gmail.com>
2025-03-08 19:53:16 +0330

    write the converter simpler and make it work again my be!

--- files/scripts/convertor.sh
+++ files/scripts/convertor.sh
@@ -12,29 +12,7 @@ if [ ! -d "$TARGET_DIR" ]; then
 fi
 
 # Find all files in the source directory
-find "$SOURCE_DIR" -type f -print0 | while IFS= read -r -d $'\0' file; do
-  # Determine if the file is an audio file (using ffmpeg's ability to probe)
-  if ffmpeg -i "$file" -f null - 2>/dev/null; then # ffmpeg returns 0 if it can read the file
-      # Extract the filename without the extension
-      filename=$(basename "$file")
-      extension="${filename##*.}"
-      filename="${filename%.*}"
+find "$SOURCE_DIR" -type f -iname "*mp3" -exec ffmpeg -y -i {} {}.opus \;
 
-      # Construct the output filename
-      output_file="$TARGET_DIR/$filename.opus"
-
-      # Convert the file using ffmpeg
-      ffmpeg -y -i "$file" -c:a libopus "$output_file"
-
-      # Check if the conversion was successful
-      if [ $? -eq 0 ]; then
-        echo "Converted: $file -> $output_file"
-      else
-        echo "Error converting: $file"
-      fi
-  else
-    echo "Skipping non-audio file: $file"
-  fi
-done
-
-echo "Conversion process completed."
+# Move all opus file to musics dir
+mv "$SOURCE_DIR"/*opus files/musics/
