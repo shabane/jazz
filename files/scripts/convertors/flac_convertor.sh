06a75edddd3175b45bdefbfbc3d5b7a61f5858a8
shabane <m.mohamadshabane@gmail.com>
2025-03-10 21:46:43 +0330

    feat: add other convetors(mp3, flac, m4a, ogg)

---
+++ files/scripts/convertors/flac_convertor.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SOURCE_DIR="./raw"
+
+# Target directory
+TARGET_DIR="files/musics"
+
+# Check if the target directory exists, create it if not
+if [ ! -d "$TARGET_DIR" ]; then
+  mkdir -p "$TARGET_DIR"
+fi
+
+# Find all files in the source directory
+find "$SOURCE_DIR" -type f -iname "*flac" -exec ffmpeg -y -i {} {}.opus \;
+
+# Move all opus file to musics dir
+mv "$SOURCE_DIR"/*opus files/musics/
